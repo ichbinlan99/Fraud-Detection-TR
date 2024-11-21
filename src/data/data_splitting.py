39afdbaa14e0c549b4765bb8f0cbd918e02650b0
binlwu <wubinlan97@gmail.com>
2024-11-21 01:50:51 +0100

    restructuring repo

--- src/data/data_splitting.py
+++ src/data/data_splitting.py
@@ -1,8 +1,8 @@
 from datetime import datetime, timedelta
 import pandas as pd
-from sklearn.preprocessing import StandardScaler
-import time
-import pickle
+import logging
+logging.basicConfig(level=logging.INFO)  # Set the log level to INFO
+logger = logging.getLogger(__name__)
 
 def create_data_splits(df, date_column, freq='90D', rolling_window=90, gap=31, val_duration=90):
     """
@@ -19,8 +19,7 @@ def create_data_splits(df, date_column, freq='90D', rolling_window=90, gap=31, v
 
     Yields:
         dict: A dictionary with training and validation data splits:
-            - 'train_df': Training features and target.
-            - 'val_df', 'y_val': Validation features and target.
+            - 'train_df', 'val_df': Training and validation DataFrames.
             - 'train_dates', 'val_dates': Date ranges for training and validation sets.
     """
     # Convert the date column to datetime and sort by it
@@ -40,75 +39,22 @@ def create_data_splits(df, date_column, freq='90D', rolling_window=90, gap=31, v
         train_start = max(train_start, start_date)
         val_end = min(val_end, end_date)
 
-        # if (train_end - train_start).days + 1 < rolling_window:
-        #     print(f"Skipping split for date {date}: Training duration is less than {rolling_window} days.")
-        # else:
-        if train_start < train_end and val_start < val_end:
-            train_df = df_sorted.loc[train_start:train_end]
-            val_df = df_sorted.loc[val_start:val_end]
-
-
-            yield {
-                'train_df': train_df,
-                'val_df': val_df,
-                'train_dates': (train_df.index.min(), train_df.index.max()),
-                'val_dates': (val_df.index.min(), val_df.index.max())
-            }
+        if (train_end - train_start).days + 1 < rolling_window:
+            logger.info(f"Skipping split for date {date}: Training duration is less than {rolling_window} days.")
+        elif (val_end - val_start).days + 1 < val_duration:
+            logger.info(f"Skipping split for date {date}: Validation duration is less than {val_duration} days.")
         else:
-            print(f"Skipping split for date {date}: Not enough data for train or validation set.")
+            if train_start < train_end and val_start < val_end:
+                train_df = df_sorted.loc[train_start:train_end]
+                val_df = df_sorted.loc[val_start:val_end]
 
 
-def scaleData(train_df, val_df, features):
-    scaler = StandardScaler()
-    train_df[features] = scaler.fit_transform(train_df[features])
-    val_df[features] = scaler.transform(val_df[features]) 
-    return train_df, val_df
-
-def fit_model_and_get_predictions(classifier, train_df, val_df, 
-                                  input_features, save_name, output_feature="is_fraud", scale=False):
-    """Trains a classifier, makes predictions, and returns the results.
-
-    Args:
-        classifier: The classifier to be trained.
-        train_df: The training dataframe.
-        val_df: The validation dataframe.
-        input_features: A list of input feature names.
-        output_feature: The name of the output feature (default: "TX_FRAUD").
-        scale: Whether to scale the input features (default: True).
-
-    Returns:
-        A dictionary containing the trained classifier, predictions, and execution times.
-        Raises ValueError if input dataframes are missing required columns.
-    """
-
-    # Input validation
-    if not all(feature in train_df.columns for feature in input_features):
-        raise ValueError("Not all input features are present in the training dataframe.")
-    if output_feature not in train_df.columns:
-        raise ValueError("Output feature is not present in the training dataframe.")
-    if not all(feature in val_df.columns for feature in input_features):
-        raise ValueError("Not all input features are present in the validation dataframe.")
-    if output_feature not in val_df.columns:
-        raise ValueError("Output feature is not present in the validation dataframe.")
-
-
-    if scale:
-        train_df, val_df = scaleData(train_df.copy(), val_df.copy(), input_features)  # Create copies to avoid modifying original dataframes
-
-    start_time = time.time()
-    classifier.fit(train_df[input_features], train_df[output_feature])
-    training_execution_time = time.time() - start_time
-
-    start_time = time.time()
-    predictions_val = classifier.predict_proba(val_df[input_features])[:, 1]  
-    prediction_execution_time = time.time() - start_time
-
-    # save
-    with open("./saved_model/classifier/" + str(save_name) + ".pkl", "wb") as f:
-        pickle.dump(classifier, f)
-
-    return {'classifier': classifier,
-            'predictions_val': predictions_val,  
-            'training_execution_time': training_execution_time,
-            'prediction_execution_time': prediction_execution_time}
+                yield {
+                    'train_df': train_df,
+                    'val_df': val_df,
+                    'train_dates': (train_df.index.min(), train_df.index.max()),
+                    'val_dates': (val_df.index.min(), val_df.index.max())
+                }
+            else:
+                print(f"Skipping split for date {date}: Not enough data for train or validation set.")
 
--- src/utils/eval_metrics.py
+++ src/utils/eval_metrics.py
@@ -1,83 +1,72 @@
-from sklearn import metrics
 import numpy as np
 import pandas as pd
+import time
+import pickle
 
-def card_precision_top_k(predictions_df, top_k, remove_detected_compromised_cards=False):
+
+def precision_top_k_day(df_day, top_k=100):
     """
-    Computes the precision at top k for credit card fraud detection on a daily basis.
-    Parameters:
-    predictions_df (pd.DataFrame): DataFrame containing transaction predictions with columns 'trans_date', 'predictions', 'cc_num', and 'is_fraud'.
-    top_k (int): The number of top predictions to consider for computing precision.
-    remove_detected_compromised_cards (bool): If True, removes detected compromised cards from future days' transactions. Default is True.
+    Calculate precision for the top K transactions of a single day.
+    
+    Args:
+        df_day (DataFrame): Data for a single day with fraud predictions.
+        top_k (int): The number of top transactions to evaluate. 
+                     If None, defaults to the number of actual frauds.
+
     Returns:
-    tuple: A tuple containing:
-        - num_compromised_cards_per_day (list): List of the number of unique compromised cards per day.
-        - card_precision_top_k_per_day_list (list): List of precision at top k values per day.
-        - mean_card_precision_top_k (float): Mean precision at top k over all days.
+        detected_fraudulent_transactions (list): List of detected fraudulent transaction IDs.
+        precision_top_k (float): Precision for the top K transactions.
     """
+    if top_k is None:
+        top_k = df_day['is_fraud'].sum()  # Default to the number of fraud cases if not specified.
 
-    # Sort days by increasing order
-    list_days=list(predictions_df['trans_date'].unique())
-    list_days.sort()
-    
-    # At first, the list of detected compromised cards is empty
-    list_detected_compromised_cards = []
-    
-    card_precision_top_k_per_day_list = []
-    num_compromised_cards_per_day = []
-    
-    # For each day, compute precision top k
-    for day in list_days:
-        
-        df_day = predictions_df[predictions_df['trans_date']==day]
-        df_day = df_day[['predictions', 'cc_num', 'is_fraud']]
-        
-        # Let us remove detected compromised cards from the set of daily transactions
-        df_day = df_day[df_day['cc_num'].isin(list_detected_compromised_cards)==False]
-        
-        num_compromised_cards_per_day.append(len(df_day[df_day['is_fraud']==1]['cc_num'].unique()))
-        
-        detected_compromised_cards, card_precision_top_k = card_precision_top_k_day(df_day,top_k)
-        
-        card_precision_top_k_per_day_list.append(card_precision_top_k)
-        
-        # Let us update the list of detected compromised cards
-        if remove_detected_compromised_cards:
-            list_detected_compromised_cards.extend(detected_compromised_cards)
-        
-    # Compute the mean
-    mean_card_precision_top_k = np.array(card_precision_top_k_per_day_list).mean()
-    
-    # Returns precision top k per day as a list, and resulting mean
-    return num_compromised_cards_per_day,card_precision_top_k_per_day_list,mean_card_precision_top_k
+    # Order transactions by descending fraud prediction probabilities.
+    df_day = df_day.sort_values(by="predictions", ascending=False).reset_index(drop=True)
 
-def performance_assessment(predictions_df, output_feature='is_fraud', 
-                           prediction_feature='predictions', top_k_list=[50],
-                           rounded=True):
+    # Select the top K most suspicious transactions.
+    df_day_top_k = df_day.head(top_k)
+
+    # Identify detected fraudulent transactions.
+    detected_fraudulent_transactions = df_day_top_k[df_day_top_k['is_fraud'] == 1]['trans_num'].tolist()
+
+    # Compute precision as the ratio of correctly identified frauds to top K.
+    precision_top_k = len(detected_fraudulent_transactions) / top_k
+
+    return detected_fraudulent_transactions, precision_top_k
+
+
+def precision_top_k(predictions_df, top_k=100):
     """
-    Assess the performance of a fraud detection model using various metrics.
-    Parameters:
-    predictions_df (pd.DataFrame): val_df containing the true labels and predicted scores.
-    output_feature (str): Column name for the true labels in predictions_df. Default is 'is_fraud'.
-    prediction_feature (str): Column name for the predicted scores in predictions_df. Default is 'predictions'.
-    top_k_list (list of int): List of top-k values for which to calculate card precision. Default is [100].
-    rounded (bool): Whether to round the performance metrics to 3 decimal places. Default is True.
+    Compute the Top K Precision metric over multiple days.
+
+    Args:
+        predictions_df (DataFrame): Data containing fraud predictions and transaction details.
+        top_k (int): The number of top transactions to evaluate per day.
+
     Returns:
-    pd.DataFrame: DataFrame containing the calculated performance metrics including AUC ROC, Average Precision, 
-        and Card Precision for each top-k value in top_k_list.
+        num_fraudulent_transactions_per_day (list): Fraud counts per day.
+        precision_top_k_per_day_list (list): Precision values for each day.
+        mean_precision_top_k (float): Mean precision across all days.
     """
-    auc_roc = metrics.roc_auc_score(predictions_df[output_feature], predictions_df[prediction_feature])
-    ap = metrics.average_precision_score(predictions_df[output_feature], predictions_df[prediction_feature])
-    f1 = metrics.f1_score(predictions_df[output_feature], predictions_df[prediction_feature].round())
-    performances = pd.DataFrame([[auc_roc, ap, f1]], 
-                           columns=['auc_roc', 'Average precision', 'F1 score'])
-    
-    # for top_k in top_k_list:
-    
-    #     _, _, mean_card_precision_top_k = card_precision_top_k(predictions_df, top_k)
-    #     performances['Card Precision@'+str(top_k)]=mean_card_precision_top_k
+    # Extract unique transaction dates.
+    unique_days = sorted(predictions_df['trans_date'].unique())
+
+    precision_top_k_per_day_list = []
+    num_fraudulent_transactions_per_day = []
+
+    # Compute precision for each day.
+    for day in unique_days:
+        df_day = predictions_df[predictions_df['trans_date'] == day][['trans_num', 'is_fraud', 'predictions']]
         
-    if rounded:
-        performances = performances.round(3)
-    
-    return performances
+        # Count the number of fraudulent transactions for the day.
+        fraud_count = df_day['is_fraud'].sum()
+        num_fraudulent_transactions_per_day.append(fraud_count)
+
+        # Compute the precision for the day.
+        _, day_precision = precision_top_k_day(df_day, top_k=top_k)
+        precision_top_k_per_day_list.append(day_precision)
+
+    # Calculate the mean precision across all days.
+    mean_precision_top_k = np.round(np.mean(precision_top_k_per_day_list), 3)
+
+    return num_fraudulent_transactions_per_day, precision_top_k_per_day_list, mean_precision_top_k
